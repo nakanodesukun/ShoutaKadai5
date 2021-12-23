0231e4b78d1cfe4aef4cf4a32fadb9226028fbb5
nakanodesukun <94818124+nakanodesukun@users.noreply.github.com>
2021-12-23 12:01:25 +0900

    Initial Commit

---
+++ 5/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  5
+//
+//  Created by 中野翔太 on 2021/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet private weak var firstTextField: UITextField!
+    @IBOutlet private weak var secondTextField: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+    
+   
+
+    @IBAction private func calculate(_ sender: Any) {
+      value()
+
+    
+     
+    }
+    
+   private func value() {
+
+        let firstNum = Float(firstTextField.text ?? "") ?? 0
+        let secondNum = Float(secondTextField.text ?? "") ?? 0
+        getValue(firstNum, secondNum)
+       
+    }
+    
+    private func getValue(_ nume1: Float, _ num2: Float) {
+        if nume1 == 0 {
+            let title = "課題５"
+            let message = "割られる数を入力して下さい"
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(ok)
+            present(alert, animated: true, completion: nil)
+
+        } else if  num2 == 0 {
+            let title = "課題５"
+            let message = "割る数を入力して下さい"
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(ok)
+            present(alert, animated: true, completion: nil)
+        }
+        resultLabel.text = String(nume1 / num2)
+    }
+
+   
+   
+}
+
+
